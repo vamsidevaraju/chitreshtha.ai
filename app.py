ece57688e681458184e761d9ae5ba22aadb02b35
vamsidevaraju <135524530+vamsidevaraju@users.noreply.github.com>
2024-05-26 13:23:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,85 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+
+
+os.environ['GOOGLE_API_KEY']= "AIzaSyAeJLYhbyiPLwwdY7ZirV7Xe36XWchS23E"
+
+genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+def get_conversational_chain():
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details. If the answer is not in
+    the provided context, just say, "answer is not available in the context", don't provide the wrong answer.\n\n
+    Context:\n{context}\n
+    Question:\n{question}\n
+    Answer:
+    """
+    
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    return chain
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    
+    # Load the FAISS index with dangerous deserialization allowed
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    
+    docs = new_db.similarity_search(user_question)
+    chain = get_conversational_chain()
+    
+    response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
+    
+    st.write("Reply:", response["output_text"])
+
+def main():
+    st.set_page_config(page_title="Chitreshtha.ai")
+    st.header("Chitreshtha.ai - Your AI Smart Tutor")
+
+    user_question = st.text_input("Ask your doubt from the context")
+    if user_question:
+        user_input(user_question)
+
+    with st.sidebar:
+        st.title("Source:")
+        pdf_docs = st.file_uploader("Upload your TextBooks in PDF format and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                try:
+                    raw_text = get_pdf_text(pdf_docs)
+                    text_chunks = get_text_chunks(raw_text)
+                    get_vector_store(text_chunks)
+                    st.success("Processing completed successfully!")
+                except Exception as e:
+                    st.error(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
